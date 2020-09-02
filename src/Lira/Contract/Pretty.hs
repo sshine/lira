bf08c9583389f8c01153ec328bd845256ba9d088
Simon Shine <shreddedglory@gmail.com>
2020-09-02 11:15:11 +0200

    Minimize parentheses in pretty-printing expressions
    Derive the method from PP using 'prettyprinter' library instead.

--- src/Lira/Contract/Pretty.hs
+++ src/Lira/Contract/Pretty.hs
@@ -39,22 +39,24 @@ pp :: Contract -> Text
 pp = renderStrict . layoutPretty defaultLayoutOptions . ppC
 
 ppC :: Contract -> Doc ann
-ppC (Transfer tok from to) = ppC' "transfer" (map pretty [pack tok, tshow from, tshow to])
-ppC (Scale maxf scalef con) = ppC' "scale" [pretty maxf, ppExpr scalef, ppC con]
-ppC (Both con1 con2) = ppC' "both" [ppC con1, ppC con2]
-ppC (Translate delay con) = ppC' "translate" [ppT delay, ppC con]
+ppC (Transfer tok from to) = ppFun "transfer" [pretty (pack tok), ppParty from, ppParty to]
+ppC (Scale maxf scalef con) = ppFun "scale" [pretty maxf, ppExpr scalef, ppC con]
+ppC (Both con1 con2) = ppFun "both" [ppC con1, ppC con2]
+ppC (Translate delay con) = ppFun "translate" [ppT delay, ppC con]
 ppC (IfWithin memExp con1 con2) = ppIfWithin memExp con1 con2
-ppC Zero = pretty @Text "zero"
+ppC Zero = pretty' "zero"
 
-ppC' :: Text -> [Doc ann] -> Doc ann
-ppC' combinator args = pretty combinator <> tupled args
+ppParty :: Party -> Doc ann
+ppParty = \case
+  Bound addr -> pretty addr
+  Free ident -> pretty' "free" <> tupled [pretty ident]
 
-ppExpr :: Expr -> Doc ann
-ppExpr = undefined
+ppFun :: Text -> [Doc ann] -> Doc ann
+ppFun combinator args = pretty combinator <> tupled args
 
 ppT :: Time -> Doc ann
 ppT = \case
-  Now -> pretty @Text "now"
+  Now -> pretty' "now"
   Seconds i -> ppT' "seconds" i
   Minutes i -> ppT' "minutes" i
   Hours i -> ppT' "hours" i
@@ -67,9 +69,93 @@ ppT' time duration =
 
 ppIfWithin :: MemExp -> Contract -> Contract -> Doc ann
 ppIfWithin (MemExp time expr) con1 con2 =
-  pretty @Text "if" <+> ppExpr expr <+> pretty @Text "within" <+> ppT time
-    <+> pretty @Text "then" <+> ppC con1
-    <+> pretty @Text "else" <+> ppC con2
+  pretty' "if" <+> ppExpr expr <+> pretty' "within" <+> ppT time
+    <+> pretty' "then" <+> ppC con1
+    <+> pretty' "else" <+> ppC con2
 
 tshow :: Show a => a -> Text
 tshow = pack . show
+
+prec :: Expr -> Int
+prec e = case e of
+  Lit _         -> 0
+  MinExp    _ _ -> 0
+  MaxExp    _ _ -> 0
+
+  MultExp   _ _ -> 1
+  DiviExp   _ _ -> 1
+  AddiExp   _ _ -> 2
+  SubtExp   _ _ -> 2
+
+  LtExp     _ _ -> 3
+  GtExp     _ _ -> 3
+  EqExp     _ _ -> 3 -- ?
+  GtOrEqExp _ _ -> 3
+  LtOrEqExp _ _ -> 3
+  NotExp _      -> 4
+  AndExp _ _    -> 5
+  OrExp  _ _    -> 6
+  IfExp _ _ _   -> 7
+
+isAssoc :: Expr -> Bool
+isAssoc e = case e of
+  MultExp _ _ -> True
+  AddiExp _ _ -> True
+  _           -> False
+
+ppBinOp :: Expr -> Expr -> Expr -> Doc ann
+ppBinOp parentE leftE rightE = ppBinOp' leftE <+> pretty op <+> ppBinOp' rightE
+ where
+  ppBinOp' :: Expr -> Doc ann
+  ppBinOp' e | prec e < prec parentE = ppExpr e
+  ppBinOp' e | prec e == prec parentE && isAssoc e && isAssoc parentE = ppExpr e
+  ppBinOp' e | otherwise = tupled [ppExpr e]
+
+  op :: Text
+  op = case parentE of
+    MultExp   _ _ -> "*"
+    DiviExp   _ _ -> "/"
+    AddiExp   _ _ -> "+"
+    SubtExp   _ _ -> "-"
+    LtExp     _ _ -> "<"
+    GtExp     _ _ -> ">"
+    EqExp     _ _ -> "="
+    GtOrEqExp _ _ -> ">="
+    LtOrEqExp _ _ -> "<="
+    AndExp    _ _ -> "and"
+    OrExp     _ _ -> "or"
+
+ppExpr :: Expr -> Doc ann
+ppExpr e = case e of
+  Lit (IntVal i) -> pretty i
+  Lit (BoolVal True) -> pretty' "true"
+  Lit (BoolVal False) -> pretty' "false"
+  Lit (Observable obsType addr key) -> pretty' "obs" <> tupled [ ppObsType obsType, pretty addr, pretty key ]
+
+  MinExp    e1 e2 -> ppFun "min" [ppExpr e1, ppExpr e2]
+  MaxExp    e1 e2 -> ppFun "max" [ppExpr e1, ppExpr e2]
+
+  MultExp   e1 e2 -> ppBinOp e e1 e2
+  DiviExp   e1 e2 -> ppBinOp e e1 e2
+  AddiExp   e1 e2 -> ppBinOp e e1 e2
+  SubtExp   e1 e2 -> ppBinOp e e1 e2
+  LtExp     e1 e2 -> ppBinOp e e1 e2
+  GtExp     e1 e2 -> ppBinOp e e1 e2
+  EqExp     e1 e2 -> ppBinOp e e1 e2
+  GtOrEqExp e1 e2 -> ppBinOp e e1 e2
+  LtOrEqExp e1 e2 -> ppBinOp e e1 e2
+  AndExp    e1 e2 -> ppBinOp e e1 e2
+  OrExp     e1 e2 -> ppBinOp e e1 e2
+
+  NotExp e1       -> ppFun "not" [ppExpr e1]
+  IfExp e1 e2 e3  -> pretty' "if" <+> tupled [ppExpr e1] <+>
+                     pretty' "then" <+> ppExpr e2 <+>
+                     pretty' "else" <+> ppExpr e3
+
+ppObsType :: ObservableType -> Doc ann
+ppObsType = \case
+  OBool -> pretty' "bool"
+  OInteger -> pretty' "int"
+
+pretty' :: Text -> Doc ann
+pretty' = pretty @Text
